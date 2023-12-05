175014eccf5a99d9b7c4bafd25155f67480dcf8e
HafizArham290 <hafizarham2006@gmail.com>
2023-12-05 23:53:13 +0500

    finding Error in output

---
+++ script.js
@@ -0,0 +1,43 @@
+let data =[
+    {
+        // "id" :"152",
+        "Name":"Abdul Wahab Amir",
+        "Father_Name": "Muhammad Amir",
+        "Education" : "Bachelors",
+        "Roll_No" :"420",
+        "Profile_Picture": "image/i1.jpg"
+    },
+{
+    // "id" :"132",
+    "Name":"Muhammad Arham",
+    "Father_Name": "Muhammad Asif",
+    "Education" : "Matric",
+    "Roll_No" :"1478330",
+    "Profile_Picture": "image/i2.jpg"
+},
+{
+    // "id" :"312",
+    "Name":"Muhammad Ibrahim",
+    "Father_Name": "Muhammad Yaqoob",
+    "Education" : "Graduation",
+    "Roll_No" :"14783123",
+    "Profile_Picture": "image/i3.jpg"
+},
+{
+    // "id" :"122",
+    "Name":"Muhammad Ahmed",
+    "Father_Name": "Muhammad Ashfaq",
+    "Education" : "Inter",
+    "Roll_No" :"121313",
+    "Profile_Picture": "image/i4.jpg"
+},
+{
+    // "id" :"352",
+    "Name":"Muhammad Shafiq",
+    "Father_Name": "Muhammad Younus",
+    "Education" : "Matric",
+    "Roll_No" :"57643",
+    "Profile_Picture": "image/i5.jpg"
+},
+
+]
